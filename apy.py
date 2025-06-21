eb652fc19a73181fb580d697a9e6e0873ca9d2ab
EmaaZmaker <emaazbinammar@gmail.com>
2025-06-21 11:20:29 +0400

    v

---
+++ apy.py
@@ -0,0 +1,12 @@
+list1=[]
+print (list1)
+list2=int(input("enter a number"))
+list3=int(input("enter another number"))
+if list2%2==0:
+    print (f"the even numbers are {list2}")
+else:
+    print(f"{list2} is not even")
+if list3%2==0:
+    print (f"{list3} is even")
+else:
+    print(f"{list3} is not even")
