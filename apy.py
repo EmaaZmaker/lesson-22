5c8ee6513dc9fdb99b2c88b3f02ee56735e87f79
EmaaZmaker <emaazbinammar@gmail.com>
2025-06-22 16:00:47 +0400

    f

--- apy.py
+++ apy.py
@@ -1,12 +1,6 @@
-list1=[]
-print (list1)
-list2=int(input("enter a number"))
-list3=int(input("enter another number"))
-if list2%2==0:
-    print (f"the even numbers are {list2}")
+
+square=int(input("enter number"))
+if square*square%2==0:
+    print("the value is even")
 else:
-    print(f"{list2} is not even")
-if list3%2==0:
-    print (f"{list3} is even")
-else:
-    print(f"{list3} is not even")
+    print("the value is odd")
